5e55876945581fa2e81e3ad479fd5c0273a5d7ef
chujingxl <chujingxl@gmail.com>
2022-08-23 00:08:48 +0800

    HomePage的Category和Headline

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,13 +4,14 @@ import ErrorToast from "../../components/ErrorToast"
 import { actions as appActions, getError } from '../../redux/modules/app';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux'
+import Home from "../Home";
 
 class App extends Component {
   render() {
     const { error, appActions: { clearError } } = this.props;
     return (
       <div className="App">
-        Hello World!
+        <Home />
         {error ? <ErrorToast msg={error} clearError={clearError} /> : null}
       </div>)
   }
